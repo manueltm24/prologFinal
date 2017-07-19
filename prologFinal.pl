892a0fb8e898ddd8c4858129f595f33864eb00c0
shantgiron <shantgiron@gmail.com>
2017-07-19 16:35:04 -0400

    Update prologFinal.pl

--- prologFinal.pl
+++ prologFinal.pl
@@ -14,269 +14,269 @@ enfermedad(gripe).
 consultarEnfermedad(X,gripe):- X = mocos, sintoma(X).
 
 %---------------------
-enfermedad(X, alergia):-
+enfermedad(alergia):-
 
-				sintoma(X, estornudos),
-				sintoma(X, tos),
-				sintoma(X, goteo),
-				sintoma(X, nasal),
-				sintoma(X, ardor_ojos),
-				sintoma(X, ardor_nariz),
-				sintoma(X, ardor_garganta),
-				sintoma(X, ojos_rojos),
-				sintoma(X, ojos_llorosos).
+				sintoma(estornudos),
+				sintoma(tos),
+				sintoma(goteo),
+				sintoma(nasal),
+				sintoma(ardor_ojos),
+				sintoma(ardor_nariz),
+				sintoma(ardor_garganta),
+				sintoma(ojos_rojos),
+				sintoma(ojos_llorosos).
 			
 
-enfermedad(X, alzheimer):-
+enfermedad(alzheimer):-
 
-				sintoma(X, perdida_de_memoria),
-				sintoma(X, dificultad_para_pensar),
-				sintoma(X, dificultad_para_hablar),
-				sintoma(X, dificultad_para_leer),
-				sintoma(X, dificultad_visual),
-				sintoma(X, confusion),
-				sintoma(X, desorentacion).
+				sintoma(perdida_de_memoria),
+				sintoma(dificultad_para_pensar),
+				sintoma(dificultad_para_hablar),
+				sintoma(dificultad_para_leer),
+				sintoma(dificultad_visual),
+				sintoma(confusion),
+				sintoma(desorentacion).
 				
 
-enfermedad(X, anemia):-
+enfermedad(anemia):-
 
-				sintoma(X, fatiga),
-				sintoma(X, falta_de_energia),
-				sintoma(X, debilidad),
-				sintoma(X, dificultad_para_respirar),
-				sintoma(X, mareos),
-				sintoma(X, palpitaciones),
-				sintoma(X, palidez),
-				sintoma(X, piel_amarilla),
+				sintoma(fatiga),
+				sintoma(falta_de_energia),
+				sintoma(debilidad),
+				sintoma(dificultad_para_respirar),
+				sintoma(mareos),
+				sintoma(palpitaciones),
+				sintoma(palidez),
+				sintoma(piel_amarilla).
 				
 
-enfermedad(X, anemia_severa):-
+enfermedad(anemia_severa):-
 
-				sintoma(X, fatiga),
-				sintoma(X, falta_de_energia),
-				sintoma(X, debilidad),
-				sintoma(X, dificultad_para_respirar),
-				sintoma(X, mareos),
-				sintoma(X, palpitaciones),
-				sintoma(X, palidez),
-				sintoma(X, piel_amarilla),
-				sintoma(X, dolor_de_pecho),
-				sintoma(X, angina),
-				sintoma(X, ataque_cardiaco).
+				sintoma(fatiga),
+				sintoma(falta_de_energia),
+				sintoma(debilidad),
+				sintoma(dificultad_para_respirar),
+				sintoma(mareos),
+				sintoma(palpitaciones),
+				sintoma(palidez),
+				sintoma(piel_amarilla),
+				sintoma(dolor_de_pecho),
+				sintoma(angina),
+				sintoma(ataque_cardiaco).
 
-enfermedad(X, anorexia):-
+enfermedad(anorexia):-
 
-				sintoma(X, cambios_de_humor),
-				sintoma(X, depresion),
-				sintoma(X, falta_de_energia),
-				sintoma(X, debilidad),
-				sintoma(X, perdida_de_memoria),
-				sintoma(X, pensamientos_lentos),
-				sintoma(X, piel_seca),
-				sintoma(X, piel_amarilla),
-				sintoma(X, unas_quebradizas),
-				sintoma(X, estrenimiento),
-				sintoma(X, hinchazon),
-				sintoma(X, caries),
-				sintoma(X, inflamacion_de_encias),
-				sintoma(X, mareos),
-				sintoma(X, desmayos),
-				sintoma(X, dolor_de_cabeza).
+				sintoma(cambios_de_humor),
+				sintoma(depresion),
+				sintoma(falta_de_energia),
+				sintoma(debilidad),
+				sintoma(perdida_de_memoria),
+				sintoma(pensamientos_lentos),
+				sintoma(piel_seca),
+				sintoma(piel_amarilla),
+				sintoma(unas_quebradizas),
+				sintoma(estrenimiento),
+				sintoma(hinchazon),
+				sintoma(caries),
+				sintoma(inflamacion_de_encias),
+				sintoma(mareos),
+				sintoma(desmayos),
+				sintoma(dolor_de_cabeza).
 
-enfermedad(X, apendicitis):-
+enfermedad(apendicitis):-
 
-				sintoma(X, falta_de_apetito),
-				sintoma(X, dolor_abdominal),
-				sintoma(X, vomitos),
-				sintoma(X, fiebre),
-				sintoma(X, colicos_abdominales).
+				sintoma(falta_de_apetito),
+				sintoma(dolor_abdominal),
+				sintoma(vomitos),
+				sintoma(fiebre),
+				sintoma(colicos_abdominales).
 
 
 				
 
-enfermedad(X, bronquiolitis):-
+enfermedad(bronquiolitis):-
 
-				sintoma(X, tos),
-				sintoma(X, mocos),
-				sintoma(X, fatiga),
-				sintoma(X, somnolencia),
-				sintoma(X, dolor_en_el_torax).
+				sintoma(tos),
+				sintoma(mocos),
+				sintoma(fatiga),
+				sintoma(somnolencia),
+				sintoma(dolor_en_el_torax).
 
 				
-enfermedad(X, chikungunya):-
+enfermedad(chikungunya):-
 
-				sintoma(X, fiebre),
-				sintoma(X, erupciones_en_la_piel),
-				sintoma(X, dolores_musculares),
-				sintoma(X, dolor_de_cabeza),
-				sintoma(X, ojos_rojos),
-				sintoma(X, dolores_articulares),
-				sintoma(X, nausea),
-				sintoma(X, cansancio).
+				sintoma(fiebre),
+				sintoma(erupciones_en_la_piel),
+				sintoma(dolores_musculares),
+				sintoma(dolor_de_cabeza),
+				sintoma(ojos_rojos),
+				sintoma(dolores_articulares),
+				sintoma(nausea),
+				sintoma(cansancio).
 
-enfermedad(X, cistitis):-
+enfermedad(cistitis):-
 
-				sintoma(X, fiebre),
-				sintoma(X, dolor_de_espalda),
-				sintoma(X, sangre_en_la_orina),
-				sintoma(X, deseos_de_orina_frecuente),
-				sintoma(X, dolor_en_los_costados).
+				sintoma(fiebre),
+				sintoma(dolor_de_espalda),
+				sintoma(sangre_en_la_orina),
+				sintoma(deseos_de_orina_frecuente),
+				sintoma(dolor_en_los_costados).
 
-enfermedad(X, conjuntivitis):-
+enfermedad(conjuntivitis):-
 
-				sintoma(X, quemazon_ocular),
-				sintoma(X, fiebre),
-				sintoma(X, lagrimeo),
-				sintoma(X, picor),
-				sintoma(X, estornudos),
-				sintoma(X, rinitis).
+				sintoma(quemazon_ocular),
+				sintoma(fiebre),
+				sintoma(lagrimeo),
+				sintoma(picor),
+				sintoma(estornudos),
+				sintoma(rinitis).
 
-enfermedad(X, colera):-
+enfermedad(colera):-
 
-				sintoma(X, diarrea),
-				sintoma(X, nauseas),
-				sintoma(X, vomitos),
-				sintoma(X, calambres_musculares),
-				sintoma(X, insuficiencia_renal),
-				sintoma(X, fiebre).
+				sintoma(diarrea),
+				sintoma(nauseas),
+				sintoma(vomitos),
+				sintoma(calambres_musculares),
+				sintoma(insuficiencia_renal),
+				sintoma(fiebre).
 
-enfermedad(X, dengue):-
+enfermedad(dengue):-
 
-				sintoma(X, fiebre_alta),
-				sintoma(X, dolor_de_cabeza),
-				sintoma(X, dolor_en_los_ojos),
-				sintoma(X, dolor_en_articulaciones),
-				sintoma(X, dolores_musculares),
-				sintoma(X, debilidad),
-				sintoma(X, nauseas),
-				sintoma(X, vomitos),
-				sintoma(X, picor).
-enfermedad(X, diabetes):-
+				sintoma(fiebre_alta),
+				sintoma(dolor_de_cabeza),
+				sintoma(dolor_en_los_ojos),
+				sintoma(dolor_en_articulaciones),
+				sintoma(dolores_musculares),
+				sintoma(debilidad),
+				sintoma(nauseas),
+				sintoma(vomitos),
+				sintoma(picor).
+enfermedad(diabetes):-
 
-				sintoma(X, erupciones_cutaneas),
-				sintoma(X, infecciones_en_la_piel),
-				sintoma(X, piel_de_atleta),
-				sintoma(X, infeccion_urinaria),
-				sintoma(X, adormecimiento_de_pies),
-				sintoma(X, vision_borrosa),
-				sintoma(X, ausencia_del_periodo),
-				sintoma(X, perdida_de_peso),
-				sintoma(X, aumento_de_peso),
-				sintoma(X, somnolencia).
+				sintoma(erupciones_cutaneas),
+				sintoma(infecciones_en_la_piel),
+				sintoma(piel_de_atleta),
+				sintoma(infeccion_urinaria),
+				sintoma(adormecimiento_de_pies),
+				sintoma(vision_borrosa),
+				sintoma(ausencia_del_periodo),
+				sintoma(perdida_de_peso),
+				sintoma(aumento_de_peso),
+				sintoma(somnolencia).
 
-enfermedad(X, embarazo):-
+enfermedad(embarazo):-
 
-				sintoma(X, falta_del_perido),
-				sintoma(X, dolor_en_los_pezones),
-				sintoma(X, nauseas),
-				sintoma(X, vomitos),
-				sintoma(X, sensibilidad_aumentada_a_los_olores),
-				sintoma(X, hinchazon_de_pechos),
-				sintoma(X, aumento_de_apetito).
-
-
-enfermedad(X, escoliosis):-
-
-				sintoma(X, hombros_desiguales),
-				sintoma(X, cintura_dispareja),
-				sintoma(X, inclinacion_hacia_un_lado),
-				sintoma(X, dolor_de_espalda),
-				sintoma(X, dificultad_para_respirar),
-				sintoma(X, perdida_de_reflejos).
-enfermedad(X, gastritis):-
-
-				sintoma(X, nauseas),
-				sintoma(X, vomitos),
-				sintoma(X, malestar_estomacal),
-				sintoma(X, dolor),
-				sintoma(X, eructos),
-				sintoma(X, hinchazon_abdominal),
-				sintoma(X, ansiedad),
-				sintoma(X, ardor_de_estomago),
-				sintoma(X, sangre_en_las_heces),
-				sintoma(X, heces_oscuras),
-				sintoma(X, perdida_de_apetito),
-				sintoma(X, fiebre),
-				sintoma(X, mal_sabor_en_la_boca),
-				sintoma(X, diarrea),
-				sintoma(X, dolor_de_pecho).
-
-enfermedad(X, gripe):-
-
-				sintoma(X, estornudos),
-				sintoma(X, tos),
-				sintoma(X, goteo),
-				sintoma(X, nasal),
-				sintoma(X, ardor_ojos),
-				sintoma(X, ardos_nariz),
-				sintoma(X, ardor_garganta),
-				sintoma(X, ojos_rojos),
-				sintoma(X, ojos_llorosos).
-
-enfermedad(X, gripe):-
-
-				sintoma(X, dolor_de_cabeza),
-				sintoma(X, fiebre),
-				sintoma(X, escalofrios),
-				sintoma(X, estornudos),
-				sintoma(X, goteo_nasal),
-				sintoma(X, inflamacion_nasal),
-				sintoma(X, nariz_tapada),
-				sintoma(X, tos),
-				sintoma(X, dolor_de_garganta),
-				sintoma(X, suforacion),
-				sintoma(X, dolores_musculares),
-				sintoma(X, perdida_del_apetito),
-				sintoma(X, fatiga),
-				sintoma(X, debilidad),
-				sintoma(X, agotamiento).
+				sintoma(falta_del_perido),
+				sintoma(dolor_en_los_pezones),
+				sintoma(nauseas),
+				sintoma(vomitos),
+				sintoma(sensibilidad_aumentada_a_los_olores),
+				sintoma(hinchazon_de_pechos),
+				sintoma(aumento_de_apetito).
 
 
-enfermedad(X, leucemia):-
+enfermedad(escoliosis):-
 
-				sintoma(X, fiebre),
-				sintoma(X, escalofrios),
-				sintoma(X, anemia),
-				sintoma(X, palidez),
-				sintoma(X, debilidad),
-				sintoma(X, fatiga),
-				sintoma(X, cansancio),
-				sintoma(X, dificultad_para_respirar),
-				sintoma(X, sangrado),
-				sintoma(X, perdida_de_apetito),
-				sintoma(X, perdida_de_peso),
-				sintoma(X, inflamacion_en_las_encias),
-				sintoma(X, sudoracion),
-				sintoma(X, dolor_en_los_huesos),
-				sintoma(X, dolor_en_articulaciones),
-				sintoma(X, encias_sangrantes).
+				sintoma(hombros_desiguales),
+				sintoma(cintura_dispareja),
+				sintoma(inclinacion_hacia_un_lado),
+				sintoma(dolor_de_espalda),
+				sintoma(dificultad_para_respirar),
+				sintoma(perdida_de_reflejos).
+enfermedad(gastritis):-
+
+				sintoma(nauseas),
+				sintoma(vomitos),
+				sintoma(malestar_estomacal),
+				sintoma(dolor),
+				sintoma(eructos),
+				sintoma(hinchazon_abdominal),
+				sintoma(ansiedad),
+				sintoma(ardor_de_estomago),
+				sintoma(sangre_en_las_heces),
+				sintoma(heces_oscuras),
+				sintoma(perdida_de_apetito),
+				sintoma(fiebre),
+				sintoma(mal_sabor_en_la_boca),
+				sintoma(diarrea),
+				sintoma(dolor_de_pecho).
+
+enfermedad(gripe):-
+
+				sintoma(estornudos),
+				sintoma(tos),
+				sintoma(goteo),
+				sintoma(nasal),
+				sintoma(ardor_ojos),
+				sintoma(ardos_nariz),
+				sintoma(ardor_garganta),
+				sintoma(ojos_rojos),
+				sintoma(ojos_llorosos).
+
+enfermedad(gripe):-
+
+				sintoma(dolor_de_cabeza),
+				sintoma(fiebre),
+				sintoma(escalofrios),
+				sintoma(estornudos),
+				sintoma(goteo_nasal),
+				sintoma(inflamacion_nasal),
+				sintoma(nariz_tapada),
+				sintoma(tos),
+				sintoma(dolor_de_garganta),
+				sintoma(suforacion),
+				sintoma(dolores_musculares),
+				sintoma(perdida_del_apetito),
+				sintoma(fatiga),
+				sintoma(debilidad),
+				sintoma(agotamiento).
+
+
+enfermedad(leucemia):-
+
+				sintoma(fiebre),
+				sintoma(escalofrios),
+				sintoma(anemia),
+				sintoma(palidez),
+				sintoma(debilidad),
+				sintoma(fatiga),
+				sintoma(cansancio),
+				sintoma(dificultad_para_respirar),
+				sintoma(sangrado),
+				sintoma(perdida_de_apetito),
+				sintoma(perdida_de_peso),
+				sintoma(inflamacion_en_las_encias),
+				sintoma(sudoracion),
+				sintoma(dolor_en_los_huesos),
+				sintoma(dolor_en_articulaciones),
+				sintoma(encias_sangrantes).
 				
-enfermedad(X, miopia):-
+enfermedad(miopia):-
 
-				sintoma(X, buena_vision_cercana),
-				sintoma(X, dificultad_para_ver_lejos).
+				sintoma(buena_vision_cercana),
+				sintoma(dificultad_para_ver_lejos).
 				
 				
-enfermedad(X, migrana):-
+enfermedad(migrana):-
 
-				sintoma(X, dolor_de_cabeza),
-				sintoma(X, nauseas),
-				sintoma(X, vomitos),
-				sintoma(X, debilidad),
-				sintoma(X, vision_borroza),
-				sintoma(X, sencibilidad_a_la_luz),
-				sintoma(X, dolor_de_ojos).
+				sintoma(dolor_de_cabeza),
+				sintoma(nauseas),
+				sintoma(vomitos),
+				sintoma(debilidad),
+				sintoma(vision_borroza),
+				sintoma(sencibilidad_a_la_luz),
+				sintoma(dolor_de_ojos).
 				
 
 
-enfermedad(X, neumonia):-
+enfermedad(neumonia):-
 
-				sintoma(X, escalofrio),
-				sintoma(X, fiebre),
-				sintoma(X, tos),
-				sintoma(X, dolor_de_pecho),
-				sintoma(X, dolor_al_respirar),
-				sintoma(X, falta_de_oxigeno),
-				sintoma(X, cansancio).
+				sintoma(escalofrio),
+				sintoma(fiebre),
+				sintoma(tos),
+				sintoma(dolor_de_pecho),
+				sintoma(dolor_al_respirar),
+				sintoma(falta_de_oxigeno),
+				sintoma(cansancio).
 				
 				

f1763eb175ae1581acd362d491620087b3655dc7
shantgiron <shantgiron@gmail.com>
2017-07-20 22:50:14 -0400

    Update prologFinal.pl

--- prologFinal.pl
+++ prologFinal.pl
@@ -1,19 +1,4 @@
-estudiante(darlenys).
-estudiante(manuel).
-estudiante(shantall).
-estudiante(jose).
-estudiante(maria).
 
-
-sintoma(mocos).
-sintoma(tos).
-sintoma(fiebre).
-enfermedad(gripe).
-
-
-consultarEnfermedad(X,gripe):- X = mocos, sintoma(X).
-
-%---------------------
 enfermedad(alergia):-
 
 				sintoma(estornudos),
@@ -279,109 +264,96 @@ enfermedad(neumonia):-
 				sintoma(falta_de_oxigeno),
 				sintoma(cansancio).
 				
-				
-%part2
-notice:-
-write('This is an Example of an Expert System for Diagnosing some Lung Diseases'),nl,
-write('Copyright (C) 2013'),nl,
-write('No rights reserved, use it as you wish!'),nl,
-nl,
-write('To use it, just answer the quizes the systems asks you.'),nl,
+
+/*Reglas para preguntas*/
+
+sintoma(Sint):-pregunta('Se siente usted con',Sint).
+pregunta(Obj, Sint):-known(Obj, Sint, true),!.
+pregunta(Obj, Sint):-known(Obj, Sint, false),!, fail.
+pregunta(Obj, Sint):-nl,write(Obj),write(' '),
+			write(Sint) , write('?(y/n)'), read(Ans), !,
+			((Ans=y, assert(known(Obj, Sint, true)));(assert(known(Obj, Sint, false)),fail)).
+			
+diagnosticar:-nl,write('Diagnosticando enfermedad..........'),nl, enfermedad(sintoma,Enfermedad) ,!,nl,
+			write('Usted posiblemente puede tener'), write(Enfermedad).
+diagnosticar:- nl, write('Lo sentimos, no tenemos una enfermedad para tus sintomas!!').
+
+start:-abolish(known/3),dynamic(known/3), retractall(known/3), diagnosticar,nl,nl, write('Intentar de nuevo ? (y/n)'),read(Resp),\+ Resp=y,
+		nl,write('Bye ! Gracias por usar el sistema'),abolish(known,3) .
+
+
+
+
+
+
+
+menu:-
+write('Sistema Experto para diagnosticar enfermedades'),nl,
+write('Responde a las preguntas que el sistema realize.'),nl,
 nl.
 
 
-%hypothesises
+%enfermedades
 
-disease(Patient,tuberculosis):-
-						symptom(Patient, persistent_cough),
-						symptom(Patient, constant_fatigue),
-						symptom(Patient, weight_loss),
-						symptom(Patient, lack_of_appetite),
-						symptom(Patient, fever),
-						symptom(Patient, coughing_blood),
-						symptom(Patient, night_sweats).
+disease(tuberculosis):-
+						symptom(persistent_cough),
+						symptom(constant_fatigue),
+						symptom(weight_loss),
+						symptom(lack_of_appetite),
+						symptom(fever),
+						symptom(coughing_blood),
+						symptom(night_sweats).
 								
-disease(Patient,pneumonia):-
-						symptom(Patient, cough),
-						symptom(Patient, fever),
-						symptom(Patient, shaking_chills),
-						symptom(Patient, shortness_of_breath).
+disease(neumonia):-
+						symptom(cough),
+						symptom(fever),
+						symptom(shaking_chills),
+						symptom(shortness_of_breath).
 							
-disease(Patient,byssinosis):-
-						symptom(Patient, chest_tightness),
-						symptom(Patient, cough),
-						symptom(Patient, wheezing).
+disease(byssinosis):-
+						symptom(chest_tightness),
+						symptom(cough),
+						symptom(wheezing).
 						
-disease(Patient,pertusis):-
-						symptom(Patient, runny_nose),
-						symptom(Patient, mild_fever).
+disease(pertusis):-
+						symptom(runny_nose),
+						symptom(mild_fever).
 						
-disease(Patient,pneumoconiosis):-
-						symptom(Patient,chronic_cough),
-						symptom(Patient,shortness_of_breath).
+disease(pneumoconiosis):-
+						symptom(chronic_cough),
+						symptom(shortness_of_breath).
 						
-disease(Patient,sarcoidosis):-
-						symptom(Patient, dry_cough),
-						symptom(Patient, shortness_of_breath),
-						symptom(Patient, mild_chest_pain),
-						symptom(Patient, scaly_rash),
-						symptom(Patient, fever),
-						symptom(Patient, red_bumps_on_legs),
-						symptom(Patient, sore_eyes),
-						symptom(Patient, swollen_ankles).
+disease(sarcoidosis):-
+						symptom(dry_cough),
+						symptom(shortness_of_breath),
+						symptom(mild_chest_pain),
+						symptom(scaly_rash),
+						symptom(fever),
+						symptom(red_bumps_on_legs),
+						symptom(sore_eyes),
+						symptom(swollen_ankles).
 						
-disease(Patient,asbestosis):-
-						symptom(Patient, chest_tightness),
-						symptom(Patient, shortness_of_breath),
-						symptom(Patient, chest_pain),
-						symptom(Patient, lack_of_appetite).
+disease(Pasbestosis):-
+						symptom(chest_tightness),
+						symptom(shortness_of_breath),
+						symptom(chest_pain),
+						symptom(lack_of_appetite).
 						
 
-disease(Patient,asthma):-
-						symptom(Patient, wheezing),
-						symptom(Patient, cough),
-						symptom(Patient, chest_tightness),
-						symptom(Patient, shortness_of_breath).
-						
-disease(Patient,bronchiolitis):-
-						symptom(Patient, wheezing),
-						symptom(Patient, fever),
-						symptom(Patient, blue_skin),
-						symptom(Patient, rapid_breath).
-						
-disease(Patient,influenza):-
-						symptom(Patient, headache),
-						symptom(Patient, fever),
-						symptom(Patient, shaking_chills),
-						symptom(Patient, nasal_congestion),
-						symptom(Patient, runny_nose),
-						symptom(Patient, sore_throat).
-						
-disease(Patient,lung_cancer):-
-						symptom(Patient, cough),
-						symptom(Patient, fever),
-						symptom(Patient, hoarseness),
-						symptom(Patient, chest_pain),
-						symptom(Patient, wheezing),
-						symptom(Patient, weight_loss),
-						symptom(Patient, lack_of_appetite),
-						symptom(Patient, coughing_blood),
-						symptom(Patient, headache),
-						symptom(Patient, shortness_of_breath).
-						
+
 
 /*Ask rules*/
 
-symptom(P, Val):-ask('Does the Patient have',Val).
+symptom(Val):-ask('Se siente usted con',Val).
 ask(Obj, Val):-known(Obj, Val, true),!.
 ask(Obj, Val):-known(Obj, Val, false),!, fail.
 ask(Obj, Val):-nl,write(Obj),write(' '),
 			write( Val) , write('?(y/n)'), read(Ans), !,
 			((Ans=y, assert(known(Obj, Val, true)));(assert(known(Obj, Val, false)),fail)).
 			
-diagnose:-nl,write('Diagnosing a lung disease..........'),nl,disease(symptom,Disease) ,!,nl,
-			write('That lung disease could be '), write(Disease).
-diagnose:- nl, write('Sorry,we may not be able to diagnose the desease!!').
+diagnose:-nl,write('Diagnosticando enfermedad..........'),nl,disease(symptom,Disease) ,!,nl,
+			write('Usted posiblemente puede tener'), write(Disease).
+diagnose:- nl, write('Lo sentimos, no tenemos una enfermedad para tus sintomas!!').
 
-start:-notice,repeat, abolish(known/3),dynamic(known/3), retractall(known/3), diagnose,nl,nl, write('Try again ? (y/n)'),read(Resp),\+ Resp=y,
-		nl,write('Bye ! Thanks for using this system'),abolish(known,3) .
+start:-menu,repeat, abolish(known/3),dynamic(known/3), retractall(known/3), diagnose,nl,nl, write('Intentar de nuevo ? (y/n)'),read(Resp),\+ Resp=y,
+		nl,write('Bye ! Gracias por usar el sistema'),abolish(known,3) .
